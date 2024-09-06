d606f66e9858387c4ddaa6bec3ed1c21ad3f549a
Juan Carlos Catagna <www.warcry@gmail.com>
2024-09-06 11:27:51 -0500

    UPDATE: Re created example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:math';
-
-import 'package:card_scanner_example/scan_option_configure_widget/scan_option_configure_widget.dart';
+import 'package:example/scan_option_configuration_widget/scan_option_configuration_widget.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -16,7 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  CardDetails _cardDetails;
+  CardDetails? _cardDetails;
   CardScanOptions scanOptions = CardScanOptions(
     scanCardHolderName: true,
     // enableDebugLogs: true,
@@ -45,7 +43,7 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              RaisedButton(
+              ElevatedButton(
                 onPressed: () async {
                   scanCard();
                 },
